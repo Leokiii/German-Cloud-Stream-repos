cddb526c1059635a2b1688f52e1a5c43c2295a70
Leokiii <164891289+Leokiii@users.noreply.github.com>
2024-04-03 16:57:59 +0200

    Aktualisieren von build.gradle.kts

--- Anime loads/build.gradle.kts
+++ Anime loads/build.gradle.kts
@@ -1,1 +1,26 @@
-h
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "de"
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+    // authors = listOf("Cloudburst")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 0 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=ofhttps://anime-loads.to&sz=%size%"
+}
